2badfa5a19280b9fda0bb2fe9c9c1d137853bdf2
JiuXia2025 <38977897+JiuXia2025@users.noreply.github.com>
2024-02-06 17:15:02 +0800

    Update switchScript.sh

--- switchScript.sh
+++ switchScript.sh
@@ -281,7 +281,7 @@ curl -sL https://api.github.com/repos/fortheusers/hb-appstore/releases/latest \
   | jq '.tag_name' \
   | xargs -I {} echo HB-Appstore {} >> ../description.txt
 curl -sL https://api.github.com/repos/fortheusers/hb-appstore/releases/latest \
-  | jq '.assets' | jq '.[0].browser_download_url' \
+  | jq '.assets' | jq '.[1].browser_download_url' \
   | xargs -I {} curl -sL {} -o appstore.nro
 if [ $? -ne 0 ]; then
     echo "HB-AppStore download\033[31m failed\033[0m."
